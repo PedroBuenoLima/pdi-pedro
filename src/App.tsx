c1fda18f16cd7ca949330646512866f9dfcdfb6e
Pedro <pedro.bueno@acct.global>
2023-05-29 11:15:02 -0300

    app created, header, banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './componentes/header/index';
 import './App.css';
+import Banner from './componentes/banner';
 
-function App() {
+const App: React.FC = () => {
+
+  const handleSearch = (searchValue: string) => {
+    // Lógica de pesquisa aqui com base no searchValue
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <Header onSearch={handleSearch}/>
+      <Banner altText="banner principal" />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/componentes/banner/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import bannerInBuzios from '../assets/bannerInBuzios.jpg'
+import './banner.scss'
+
+
+interface BannerProps {
+  altText: string;
+}
+
+const Banner: React.FC<BannerProps> = ({ altText }) => {
+    return (
+      <div className="banner">
+        <img src={bannerInBuzios} alt={altText} />
+      </div>
+    );
+  };
+  
+export default Banner;
+  
