0145852d90250ea563c153dd316a7acf0b0996da
Pedro <pedro.bueno@acct.global>
2023-05-30 09:44:42 -0300

    minor css changes

--- src/componentes/productCard/productList.tsx
+++ src/componentes/productCard/productList.tsx
@@ -10,15 +10,20 @@ interface ProductListProps {
 const ProductList: React.FC<ProductListProps> = ({ handleAddToCart }) => {
   const products = [
     { id: 1, name: 'CITY TOUR CORCOVADO', price: 10, imageUrl: corcovado, content: 'Conheça o Rio em 5h e leve muita história para casa!' },
-    { id: 2, name: 'Product 2', price: 20, imageUrl: 'url-da-imagem-2', content: ' llala' },
-    // Outros produtos...
+    { id: 2, name: 'PASSEIO DE BARCO EM ARRAIAL', price: 20, imageUrl: 'url-da-imagem-2', content: 'Um dia inesquecível !' },
+    { id: 3, name: 'CITY TOUR BÚZIOS', price: 30, imageUrl: 'url-da-imagem-2', content: 'Um dia em Búzios com passeio de escuna!' },
+    { id: 4, name: 'CITY TOUR RIO', price: 40, imageUrl: 'url-da-imagem-2', content: 'Conheça os principais pontos turísticos da "Cidade Maravilhosa"' },
+    { id: 5, name: 'CITY TOUR CABO FRIO', price: 50, imageUrl: 'url-da-imagem-2', content: 'Um dia inesquecível !' },
   ];
 
   return (
-    <div className='product-carousel'>
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} addToCart={handleAddToCart} />
-      ))}
+    <div>
+      <h1 className='product-carousel-title'>Nossos Passeios</h1>
+      <div className='product-carousel'>
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} addToCart={handleAddToCart} />
+        ))}
+      </div>
     </div>
   );
 };
